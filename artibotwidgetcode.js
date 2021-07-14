41862cd2ec2ab0a794db0e0702eb879ace71586d
Boddeti priyanka <58573869+priyankaboddeti@users.noreply.github.com>
2021-07-14 06:02:57 +0530

    Initial commit

---
+++ artibotwidgetcode.js
@@ -0,0 +1,1 @@
+!function(t){var e=!1,a=t.createElement("script");a.async=!0,a.type="text/javascript",a.src="https://artibot.ai/loader.js",t.getElementsByTagName("head").item(0).appendChild(a),a.onreadystatechange=a.onload=function(){if(!(e||this.readyState&&"loaded"!=this.readyState&&"complete"!=this.readyState)){new window.ArtiBot({i:artibot_widget_object.replacement_code});e=!0}}}(document);
